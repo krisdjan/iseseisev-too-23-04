7c2c00d22d73763eebfcd209644382cc83dceb8b
Kristjan Sarv <146323487+krisdjan@users.noreply.github.com>
2024-05-01 21:44:20 +0300

    failid

---
+++ src/amplitude.js
@@ -0,0 +1,30 @@
+Amplitude.init({
+    "bindings": {
+      37: 'prev',
+      39: 'next',
+      32: 'play_pause'
+    },
+    "songs": [
+      {
+        "name": "lihtsalt prügi",
+        "artist": "krisdjan",
+        "album": "1",
+        "url": "src/L.mp3",
+        "cover_art_url": "pics/img.JPEG"
+      }
+    ]
+  });
+
+  window.onkeydown = function(e) {
+      return !(e.keyCode == 32);
+  };
+
+  /*
+    Handles a click on the song played progress bar.
+  */
+  document.getElementById('song-played-progress').addEventListener('click', function( e ){
+    var offset = this.getBoundingClientRect();
+    var x = e.pageX - offset.left;
+
+    Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+  })
---
+++ src/script.js
@@ -0,0 +1,9 @@
+new fullpage("#fullpage", {
+    anchors: ['pealeht', 'oskused', 'kontakt', 'muusika', 'hobid', 'tood', 'mang'],
+    autoScrolling:true,
+    navigation: false,
+    menu: '#menu',
+    scrollingSpeed: 400
+    });
+
+    
